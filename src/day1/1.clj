500550b97a4122b986884e927abd04d62f049427
Paul Horikx <p.horikx@gmail.com>
2024-12-01 11:29:25 +0100

    day 1 a finished

---
+++ src/day1/1.clj
@@ -0,0 +1,22 @@
+(require '[clojure.string :as str])
+
+(defn build-lists [file]
+  (let [lines (str/split-lines file)
+        split-lines (mapv #(str/split % #"\s+") lines)
+        list1 (mapv #(Integer/parseInt (get % 0)) split-lines)
+        list2 (mapv #(Integer/parseInt (get % 1)) split-lines)]
+    (vector list1 list2)))
+
+(defn get-answer [list1 list2 total]
+  (if (empty? list1) total
+      (recur (vec (butlast list1)) (vec (butlast list2)) (+ total (Math/abs (- (last list1) (last list2)))))))
+
+(defn run []
+  (let [input (slurp "./src/day1/input.txt")
+        lists (build-lists input)
+        sorted-lists (mapv #(sort %) lists)
+        [list1 list2] sorted-lists
+        answer (get-answer list1 list2 0)]
+    (println answer)))
+
+(run)
